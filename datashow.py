8c3f028461109a921fb844cd5456b6051b935d86
zhudingsuifeng <1002557401@qq.com>
2017-12-13 11:07:51 +0800

    show distrribution of similarity

--- datashow.py
+++ datashow.py
@@ -2,7 +2,7 @@
 #coding=utf-8
 """
 Create on Thu Aug 24 21:01:35 2017
-Modified on Wed Sep 27 11:22:00 2017
+Modified on Mon Dec 4 09:43:26 2017
 @author: fly
 Compare picture various similarity and seve to file.
 """
@@ -18,14 +18,15 @@ import pandas as pd
 
 #compare image similarity
 def getdata(datapath):
-    path=os.path.join('../data',datapath) #combine the file path
-    data=np.load(path)    #load the npy file
-    floatdata=[]
-    return map(float,data[0][1:])     #map(func,data)   #data is a array-like series
+    path=os.path.join('/home/fly/mygit/data/similarity',datapath) #combine the file path
+    data=np.loadtxt(path,delimiter=",")    #load the csv file
+    return data[0]
+    #floatdata=[]
+    #return map(float,data[0][1:])     #map(func,data)   #data is a array-like series
 
 if __name__=="__main__":
     wn.filterwarnings("ignore")
-    filelist=os.listdir('../data')
+    filelist=os.listdir('/home/fly/mygit/data/similarity')
     label=[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
     for name in range(0,len(filelist)):
 	bardata=[]
@@ -43,9 +44,10 @@ if __name__=="__main__":
 	plt.bar(np.arange(10),bardata,facecolor='g')    #draw the bar image
 	plt.xticks(np.arange(10),('0.0~0.1','0.1~0.2','0.2~0.3','0.3~0.4','0.4~0.5','0.5~0.6','0.6~0.7','0.7~0.8','0.8~0.9','0.9~1.0'),rotation=30)
 	#plt.xticks()  #set the xticks ,rotation=90,rotate 30 degrees
-	path=os.path.join('../images',shortname+'.png')
+	path=os.path.join('/home/fly/mygit/images/similarity',shortname+'.png')
 	plt.savefig(path,dpi=100)
 	#plt.show()
+	plt.close()
 	#break
 	
     print("success")
