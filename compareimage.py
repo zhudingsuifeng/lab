bbe857b26c66834137797b9baeefd202a95cb4ca
zhudingsuifeng <1002557401@qq.com>
2017-12-13 11:09:14 +0800

    calculate the structural similarity

--- compareimage.py
+++ compareimage.py
@@ -25,21 +25,21 @@ def mse(imageA,imageB):
     return err
 
 #save compare result to file
-def save(ssim,nrmse,savedir,title):
-    for inputarray,category in (ssim,'ssim'),(nrmse,'nrmse'):
-	path=os.path.join(savedir,title+category+".csv")         #combine path
-	np.savetxt(path,inputarray,delimiter=',')     #save to npy file,converted data to string by default.
+def save(ssim,savedir,title):
+    #for inputarray,category in (ssim,'ssim'):
+    path=os.path.join(savedir,title+"ssim.csv")         #combine path
+    np.savetxt(path,ssim,delimiter=',')     #save to npy file,converted data to string by default.
     print("Save "+title+" success!")
 
 #compare image similarity
 def compare(imagedir):
     ssimarray=[]
-    nrmsearray=[]
+    #nrmsearray=[]
     filelist=os.listdir(imagedir)      #list files of the dir
     for name in range(0,len(filelist)):
 	(shortname,extension)=os.path.splitext(filelist[name])
 	ssimarray.append([])
-	nrmsearray.append([])
+	#nrmsearray.append([])
     for imdir in range(0,len(filelist)):
 	imagepath=os.path.join(imagedir,filelist[imdir]) #combine the file path
 	original=cv2.imread(imagepath)    #load the images
@@ -50,41 +50,24 @@ def compare(imagedir):
 	    #secimage=cv2.cvtColor(secimage,cv2.COLOR_BGR2GRAY)
 	    ssimvalue=sm.compare_ssim(original,secimage,multichannel=True)   #ssim
 	    #psnrvalue=sm.compare_psnr(original,secimage)                     #psnr  
-	    nrmsevalue=sm.compare_nrmse(original,secimage)                    #nrmse   
+	    #nrmsevalue=sm.compare_nrmse(original,secimage)                    #nrmse   
 	    print(filelist[secimdir]+" complete!ssim value is "+str(ssimvalue))
 	    ssimarray[imdir].append(ssimvalue)
-	    nrmsearray[imdir].append(nrmsevalue)
+	    #nrmsearray[imdir].append(nrmsevalue)
 	print("one success!")
 	#break
-    return ssimarray,nrmsearray
+    return ssimarray#,nrmsearray
 
 if __name__=="__main__":
     wn.filterwarnings("ignore")
 
     #savedir='/home/fly/mygit/data'
-    savedir='/home/fly/mygit/data/20/sim'
-    nrmse=[]     #nrmse
+    savedir='/home/fly/mygit/data/similarity'
+    #nrmse=[]     #nrmse
     ssim=[]      #ssim
-    '''
-    ssim,nrmse=compare('/home/fly/mygit/images/mixing/chtrcl')
-    save(ssim,nrmse,savedir,"chtrcl")
-    ssim,nrmse=compare('/home/fly/mygit/images/mixing/clchtr')
-    save(ssim,nrmse,savedir,"clchtr")
-    ssim,nrmse=compare('/home/fly/mygit/images/mixing/clclch')
-    save(ssim,nrmse,savedir,"clclch")
-    ssim,nrmse=compare('/home/fly/mygit/images/mixing/cltrch')
-    save(ssim,nrmse,savedir,"cltrch")
-    save(ssim,nrmse,savedir,"clchtr")
-    ssim,nrmse=compare('/home/fly/mygit/images/mixing/clchcl')
-    save(ssim,nrmse,savedir,"clchcl")
-    ssim,nrmse=compare('/home/fly/mygit/images/mixing/trclcl')
-    save(ssim,nrmse,savedir,"trclcl")
-    '''
-    ssim,nrmse=compare('/home/fly/mygit/data/20/images/mixing/chcltr')
-    save(ssim,nrmse,savedir,"chcltr")
-    ssim,nrmse=compare('/home/fly/mygit/data/20/images/mixing/gadf/chcltr')
-    save(ssim,nrmse,savedir,"gadfchcltr")
-    ssim,nrmse=compare('/home/fly/mygit/data/20/images/mixing/gasf/chcltr')
-    save(ssim,nrmse,savedir,"gasfchcltr")
+    ssim=compare('/home/fly/mygit/images/mixing/chcltr/gadf')
+    save(ssim,savedir,"gadf")
+    ssim=compare('/home/fly/mygit/images/mixing/chcltr/gasf')
+    save(ssim,savedir,"gasf")
 
     print("success")
