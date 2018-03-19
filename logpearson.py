cebda20aab9bf9ed0b32f5b65c7cd543adf6bca6
zhudingsuifeng <1002557401@qq.com>
2018-03-19 15:20:12 +0800

    logarithmic return index and Pearson similarity

--- logpearson.py
+++ logpearson.py
@@ -2,9 +2,11 @@
 #coding=utf-8
 """
 Create on Mon Oct 30 21:46:32 2017
-Modify on Wed Dec 20 16:31:19 2017
+Modify on Mon Mar 19 10:20:46 2018
 @author:fly
-Get stock close,calculate logarighmic price earnings and the pearson correlation coefficient of it and save as csv file.
+Galculate logarighmic price earnings and the pearson correlation coefficient of it and save as csv file.
+closefile:/home/fly/mygit/data/stock/close.csv
+raw stock data:/home/fly/code/dataset
 """
 import os
 import csv
@@ -13,23 +15,14 @@ import math
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-#Reverse the sequence
-def replace(series):
-    temp=[]
-    for i in range(len(series)-1,-1,-1):
-	temp.append(float(series[i]))
-    return temp
-
-#get column data from specify directory file.
-def getcoldata(datadir,name,col):
-    data=[]
-    path=os.path.join(datadir,name)
-    with open(path) as csvf:
-	reader=csv.reader(csvf)
-	for row in reader:
-	    data.append(row[col])
-    csvf.close()
-    return replace(data[1:101])
+#get stock code from stock data.
+def getstockcode(datadir):
+    stockcode=[]
+    filelist=os.listdir(datadir)              #list the file of the directory
+    for each in filelist:
+	(code,ext)=os.path.splitext(each)     #separate file names and extensions
+	stockcode.append(code)
+    return stockcode
 
 #calculate the logarighmic price earnings of stock.
 def logearn(stockprice):
@@ -47,31 +40,29 @@ def savefile(savedir,filename,data):
 
 
 if __name__=="__main__":
-    datadir='/home/fly/mygit/filter/stock'
-    savedir='/home/fly/mygit/filter/result'
-    filelist=os.listdir(datadir)   #list the file of the directory
-    close=[]
+    data='/home/fly/mygit/data/stock/close.csv'
+    stockdir='/home/fly/mygit/data/stock'
+    savedir='/home/fly/mygit/data/similarity'
     logs=[]
 
     #get stock close price.
-    for i in range(0,len(filelist)):
-	closecol=getcoldata(datadir,filelist[i],3)
-	close.append(closecol)
-    savefile(savedir,'close.csv',close)       #save stock close price to csv file
+    close=np.loadtxt(data,delimiter=",")
     print("get stock close success")
 
     #calculate the logarithmic price earnings.
     for i in range(0,len(close)):
 	log=logearn(close[i])
 	logs.append(log)
-    savefile(savedir,'logearn.csv',logs)      #save logarithmic price earnings to csv file.
+    savefile(stockdir,'logearn.csv',logs)      #save logarithmic price earnings to csv file.
     print("calclate logearn success")
 
     #calculate the pearson correlation coefficients.
     pearson=np.corrcoef(logs)    #return Pearson correlation coefficients.
+    '''
     for j in range(0,len(pearson)):
 	for i in range(0,len(pearson)):
 	    pearson[j][i]=("%.3f" % pearson[j][i]) #keep 3 decimal places.
+    '''
     savefile(savedir,'pearson.csv',pearson)
     print("calculate pearson success")
 
