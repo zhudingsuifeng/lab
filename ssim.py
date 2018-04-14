0d707157fff744e2545deea8d898ff788696b091
zhudingsuifeng <1002557401@qq.com>
2018-04-14 10:34:00 +0800

    get stock code and proccess stock data

--- ssim.py
+++ ssim.py
@@ -1,82 +1,66 @@
 #!/usr/bin/env python
 #coding=utf-8
 """
-Create on Tue Dec 12 10:51:01 2017
-Modify on Tue Dec 19 19:43:22 2017
+Create on Thu Aug 24 21:01:35 2017
+Modified on Wed Sep 27 11:22:00 2017
 @author: fly
-Find the high ssim value images.
-opencv3.3.0
-numpy1.13.1
-pandas0.20.3
-PIL4.2.1
-skimage0.13.0
-matplotlib(2.0.2)
+Compare picture various similarity and seve to file.
 """
 import os
 import csv
-import sys
 import numpy as np
-import tushare as ts
 import matplotlib.pyplot as plt
 import warnings as wn
 import cv2
 import skimage.measure as sm
 import networkx as nx
 import pandas as pd
-from PIL import Image
-from compiler.ast import flatten
 
-#get stock corresponding line
-def getline(code,stocklist):
-    for i in range(0,len(stocklist)):
-	if code==stocklist[i]:      #stock name---------------------------------------------------
-	    return i
+#save compare result to file
+def save(ssim,savedir,title):
+    #for inputarray,category in (ssim,'ssim'):
+    path=os.path.join(savedir,title+"ssim.csv")         #combine path
+    np.savetxt(path,ssim,delimiter=',')     #save to npy file,converted data to string by default.
+    print("Save "+title+" success!")
+
+#compare image similarity
+def compare(imagedir):
+    ssimarray=[]
+    filelist=os.listdir(imagedir)      #list files of the dir
+    for name in range(0,len(filelist)):
+	#(shortname,extension)=os.path.splitext(filelist[name])
+	ssimarray.append([])
+    for imdir in range(0,len(filelist)):
+	imagepath=os.path.join(imagedir,filelist[imdir]) #combine the file path
+	original=cv2.imread(imagepath)    #load the images
+	for secimdir in range(0,len(filelist)):
+	    secimagepath=os.path.join(imagedir,filelist[secimdir])
+	    secimage=cv2.imread(secimagepath)
+	    ssimvalue=sm.compare_ssim(original,secimage,multichannel=True)   #ssim
+	    #print(filelist[secimdir]+" complete!ssim value is "+str(ssimvalue))
+	    ssimarray[imdir].append(ssimvalue)
+	print("one compare success!")
+	#break
+    return ssimarray
+
+#get stock code
+def get_stock_code(stockdir):
+    filelist=os.listdir(stockdir)
+    stockcode=[]
+    for i in range(0,len(filelist)):
+	(sn,ext)=os.path.splitext(filelist[i])
+	stockcode.append(sn)
+    with open('/home/fly/hs/interdata/stockcode.csv','w') as csvf:
+	writer=csv.writer(csvf)
+	writer.writerow(stockcode)
+	csvf.close()
 
 if __name__=="__main__":
     wn.filterwarnings("ignore")
-    print("Test case.")
-    imgdir='/home/fly/mygit/filter/images'
-    #datadir='/home/fly/mygit/filter/result/pearson.csv'
-    datadir='/home/fly/mygit/filter/result/platessim.csv'
-    #ssimdir='/home/fly/mygit/filter/sort/pearson'
-    ssimdir='/home/fly/mygit/filter/sort/ssim'
-    filelist=os.listdir(imgdir)          #list the file name in the dir
+    savedir='/home/fly/hs/interdata'
+    ssim=[]      #ssim
+    ssim=compare('/home/fly/hs/images')
+    save(ssim,savedir,"gasf")
+    get_stock_code('/home/fly/hs/data')
 
-    #load ssim csv file
-    ssim=np.loadtxt(open(datadir),delimiter=",",skiprows=0)
-    print("load file success")
-
-    #get stock code
-    stockcode=[]
-    for i in range(0,len(filelist)):
-	(code,ext)=os.path.splitext(filelist[i])
-	stockcode.append(code)
-    print("get stock code")
-	
-    for row in range(0,len(ssim)):
-	ssimline=ssim[row]
-    	#merge the stock information.
-    	stockinf=[]
-    	#stockinf=ts.get_stock_basics()   #get stock data from tushare
-    	with open("/home/fly/mygit/data/stock/industry.csv","r") as csvf: #open industry.csv file.
-	    reader=csv.reader(csvf)
-	    for line in reader:
-	    	#merge the stock data.
-	    	for i in range(0,len(stockcode)):
-		    if stockcode[i]==line[1]:
-		    	stockinf.append([line[1],line[2],ssimline[i],line[3]])
-	    		#break
-    	print("merge success")
-     
-    	#save data as csv file.
-    	(shortname,ext)=os.path.splitext(filelist[row])   #save path--------------------------------------
-    	path=os.path.join(ssimdir,shortname+'ssimsort.csv')
-    	csvf=open(path,"w")   #open csv file
-    	writer=csv.writer(csvf)
-    	sortstock=sorted(stockinf,key=lambda x : x[2],reverse=True)    #sort the data
-    	for i in range(0,60):   #60 high suitability stock
-	    writer.writerow(sortstock[i])     #write data row to csv file
-    	csvf.close()    #close csv file 
-    	print("save "+str(row)+" file success")
-    
     print("success")
